23db46169b474bc6982db46be5972dad8c14b163
Crowdbotics <team@crowdbotics.com>
2020-10-20 16:05:36 +0000

    Update models

--- backend/hello_movile_20_dev_13651/wsgi.py
+++ backend/hello_movile_20_dev_13651/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_movile_20_dev_13651.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello_movile_20_dev_13651.settings")
 
 application = get_wsgi_application()
